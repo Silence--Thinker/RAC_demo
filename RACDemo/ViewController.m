ba1cd0b29b4108050906669746b9fa51fa78b9b9
silence_xjc <154261688@qq.com>
2017-10-09 17:49:42 +0800

    补充

--- RACDemo/ViewController.m
+++ RACDemo/ViewController.m
@@ -19,9 +19,13 @@
 //    [self racsignal_demo_01];
 //    [self racsubject_demo_01];
     [self racreplaysubject_demo_01];
+    [self rac_define_demo_01];
 }
 
 // RACSignal 信号 RACDisposable
+// 调用遵循：创建信号==>>订阅信号==>>订阅回调中发送信号 规则 创建信号时便保存了订阅信号的回调 拿到中间订阅者
+// 不可在其他地方拿到订阅者发送信号 顺序不可逆
+// 订阅信号时，会调用整个didSubscribe Block(订阅block) 回调
 - (void)racsignal_demo_01 {
     // racsignal 内部会有生成一个消息的订阅者，而且这个订阅者是直接拿不到的。
     // 所以发送信号也只能在create 信号的时候去做
@@ -37,7 +41,7 @@
         [subscriber sendNext:@"xxx"];
         
         // 如果不在发送数据，最好发送信号完成，内部会调用[RACDisposable dispose]取消订阅信号
-//        [subscriber sendCompleted];
+        [subscriber sendCompleted];
         
         return [RACDisposable disposableWithBlock:^{
            // block 调用时刻：当信号发送完成或者发送错误，会自动执行这个block，取消订阅信号
@@ -57,16 +61,19 @@
     }];
   /*
 log:
-    11111111
-    222222222
-    222222222 x is what xxx
-    11111111
-    333333333
-    333333333 x is what xxx
+   11111111
+   222222222
+   222222222 x is what xxx
+   取消订阅信号
+   11111111
+   333333333
+   333333333 x is what xxx
+   取消订阅信号
    */
 }
 
 // RACSubject 信号，既可以发送信号，也可以订阅信号
+// 创建信号==>>订阅信号==>>发送信号  顺序不可逆
 - (void)racsubject_demo_01 {
     // RACSubject:底层实现和RACSignal不一样。
     // 1.调用subscribeNext订阅信号，只是把订阅者保存起来，并且订阅者的nextBlock已经赋值了。
@@ -74,6 +81,9 @@ log:
     
     // 创建信号
     RACSubject *subject = [RACSubject subject];
+    
+    // 发送信号
+    [subject sendNext:@"发送信号 8989"];
     
     // 订阅信号
     [subject subscribeNext:^(id x) {
@@ -85,16 +95,22 @@ log:
     }];
     
     // 发送信号
-    [subject sendNext:@"信号东东"];
+    [subject sendNext:@"发送信号 1010"];
+    [subject sendNext:@"发送信号 6767"];
     
     /*
 log:
-    订阅信号 111111 信号东东
-    订阅信号 222222 信号东东
+     订阅信号 111111 发送信号 1010
+     订阅信号 222222 发送信号 1010
+     订阅信号 111111 发送信号 6767
+     订阅信号 222222 发送信号 6767
      */
 }
 
 // ARCReplaySubject 不管调用的顺序只管结果。。一点一览无余
+// ARCReplaySubject 会将订阅信号的回调，发送的信号都储存起来，调用顺序可逆
+// 一旦有订阅新的信号，会去寻找当前已经发送的信号，有信号，然后调用订阅的信号回调，没有就算了
+// 一旦有发送新的信号，会去寻找当前已经订阅的订阅回调，有订阅回调，调用订阅回调
 - (void)racreplaysubject_demo_01 {
     // RACReplaySubject:底层实现和RACSubject不一样。
     // 1.调用sendNext发送信号，把值保存起来，然后遍历刚刚保存的所有订阅者，一个一个调用订阅者的nextBlock。
@@ -105,33 +121,34 @@ log:
         NSLog(@"订阅信号 333333 %@", x);
     }];
     // 发送信号
-    [replaySubject sendNext:@"信号111"];
-    [replaySubject sendNext:@"信号222"];
+    [replaySubject sendNext:@"##信号111"];
+    [replaySubject sendNext:@"##信号222"];
     
     // 订阅信号
-    [replaySubject subscribeNext:^(id x) { // 这个订阅先调用两次
+    [replaySubject subscribeNext:^(id x) {
        NSLog(@"订阅信号 111111 %@", x);
     }];
     
-    [replaySubject subscribeNext:^(id x) { // 这个订阅再调用两次
+    [replaySubject subscribeNext:^(id x) {
         NSLog(@"订阅信号 222222 %@", x);
     }];
+    
+    [replaySubject sendNext:@"##信号333"];
     /*
 log:
-     订阅信号 333333 信号111
-     订阅信号 333333 信号222
-     订阅信号 111111 信号111
-     订阅信号 111111 信号222
-     订阅信号 222222 信号111
-     订阅信号 222222 信号222
+     订阅信号 333333 ##信号111
+     订阅信号 333333 ##信号222
+     订阅信号 111111 ##信号111
+     订阅信号 111111 ##信号222
+     订阅信号 222222 ##信号111
+     订阅信号 222222 ##信号222
+     订阅信号 333333 ##信号333
+     订阅信号 111111 ##信号333
+     订阅信号 222222 ##信号333
      */
 }
 
-
-- (void)sss {
-    
-}
-
+// RACSubject 替代代理
 - (IBAction)didClickBtn:(id)sender {
     NextViewController *next = [[NextViewController alloc] init];
     next.signalSubject = [RACSubject subject];
@@ -139,5 +156,12 @@ log:
         [self.button setTitle:x forState:UIControlStateNormal];
     }];
     [self.navigationController pushViewController:next animated:YES];
+}
+
+- (void)rac_define_demo_01 {
+    int a = 4, b = 0;
+    int c = ((void)(b = b > 0 ? b : a), b); // (a, b) 只会取后面的值，当时会先进行前者的运算
+    NSLog(@"%zd", b);
+    NSLog(@"%zd", c);
 }
 @end
---
+++ RAC_使用注意.md
@@ -0,0 +1,83 @@
+OSSAskAndAnswerModule
+
+RAC(self,merchant) = NVWBObserve(self, merchant);
+
+那个RAC这样写是没有问题的
+原因如下：
+关于两次调用接口的修改的解释：
+刚进入页面没有数据 调用逻辑
+
+通过协议进入这个POI页面时。走的是initWithMerchant:默认初始化了merchant 并将协议的携带的poi_id、stid、ct_poi 赋值到了merchant中，
+所以VC一初始化merchant就有了值
+
+最重要的问题在这一句
+RAC(self, merchant) = NVWBObserve(self, merchant);
+看下NVWBObserve的定义
+
+<pre><code>
+#define NVWBObserve(TARGET, KEYPATH) \
+[self.whiteBoard signalForKey:@keypath(TARGET, KEYPATH)]
+</code></pre>
+
+这句话是将self的merchant的值和self.whiteBoard的merchant值做对应，即：self.whiteBoard的merchant值改变那么self的merchant的值就等于它。并且携带初始值(具体的宏定义有点复杂，我看了结果，有空解开看下吧)。
+OverseaShopInfoViewController.m ==>>merchant初始化==>>modules==>>子控件的setupModule==>>请求数据
+完全不会在请求数据是有空的merchant。。
+=============
+大家可以放心给我A了
+
+initWithMerchant:(OverseaMerchant *)merchant
+p_requestExtraData
+
+self.whiteBoard[@keypath(self, merchant)] = merchant;
+
+NVWhiteBoard *whiteBoard
+NSMutableDictionary *subjects;
+NVWhiteBoardSubject *subject = [self subjectForKey:key];
+NVWhiteBoardSubject : RACSubject
+
+(void)setValue:(id)value forKey:(NSString *)key
+[[self subjectForKey:key] sendNext:value];
+(void)setObject:(nullable id)object forKeyedSubscript:(nonnull NSString *)key
+
+
+ //keypath(...)
+//    metamacro_if_eq(1, metamacro_argcount(__VA_ARGS__))(keypath1(__VA_ARGS__))(keypath2(__VA_ARGS__))
+//    keypath2(OBJ, PATH)
+//    (((void)(NO && ((void)self.merchant, NO)), "merchant"))
+
+<pre><code>
+metamacro_argcount(a, b, c);
+    
+#define metamacro_argcount(...) \
+    metamacro_at(20, __VA_ARGS__, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+    metamacro_at(20, a, b ,c , 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+    
+#define metamacro_at(N, ...) \
+    metamacro_concat(metamacro_at, N)(__VA_ARGS__)
+    metamacro_concat(metamacro_at, 20)(a, b, c, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+    
+#define metamacro_concat(A, B) \
+    metamacro_concat_(A, B)
+#define metamacro_concat_(A, B) A ## B
+    metamacro_at20(a, b, c, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+    
+#define metamacro_at20(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, ...) metamacro_head(__VA_ARGS__)
+    metamacro_head(3, 2, 1);
+    
+#define metamacro_head(...) \
+    metamacro_head_(__VA_ARGS__, 0)
+    metamacro_head_(3, 2, 1, 0);
+    
+#define metamacro_head_(FIRST, ...) FIRST
+    int x = metamacro_head_(3, 2, 1, 0); // x = 3
+
+
+RACSubscriptingAssignmentTrampoline
+// 非正式协议 下标设置值
+- (void)setObject:(RACSignal *)signal forKeyedSubscript:(NSString *)keyPath;
+
+</code></pre>
+
+### keypath(...) 宏解析
+
+### RAC() RAC_ 宏解析
